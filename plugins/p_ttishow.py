efd5b0a1121b3186a9f30078de973118834c8ad8
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-02-22 22:21:15 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK
+from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK,RLU_LNK
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings
@@ -64,7 +64,8 @@ async def save_group(bot, message):
                                                                            InlineKeyboardButton('📽 Fɪʟᴍs 📽', url=GRP_LNK),
                                                                            InlineKeyboardButton('🎞 Wᴇʙ Sᴇʀɪᴇs 🎞', url=SER_LNK),
                                                                            InlineKeyboardButton('🎬 Aɴɪᴍᴇ Sᴇʀɪᴇs 🎬', url=ANI_LNK)
-                                                                        ]]
+                                                                        ],[ InlineKeyboardButton('📜 Gʀᴏᴜᴘ Rᴜʟᴇs 📜', url=RLU_LNK)
+                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
                 )
